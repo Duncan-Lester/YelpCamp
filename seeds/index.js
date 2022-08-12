2a27cf545f01bf7e037ed686fdf854af637952e0
Duncan Lester <lesterduncan99@gmail.com>
2022-08-12 10:44:33 -0700

    added lowercase c in models

--- seeds/index.js
+++ seeds/index.js
@@ -17,7 +17,6 @@ db.once('open', ()=>{
 const sample = array => array[Math.floor(Math.random()* array.length)];
 
 const seedDB = async ()=>{
-    await Campground.deleteMany({});
     for(let i=0; i<50; i++){
        const random1000 = Math.floor(Math.random() * 1000);
        const price = Math.floor(Math.random()*20)+ 10;
